afb288bd60759ab970ce348f552d418d3d677409
murakami77 <murakami77@nifty.com>
2023-05-07 16:54:41 +0900

    Amplifyのフロントエンドの設定

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 
 import './assets/main.css'
 
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 createApp(App).mount('#app')
