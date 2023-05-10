6cf9ab37b2a345caa9ca0ab5126effc4fc78c4e6
murakami <murakami@bitstar.jp>
2023-05-11 00:49:42 +0900

    チュートリアル、および、Amplify Dev Centerのページを参考に、Todo管理のページに認証機能を追加

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
+<script setup>
+  import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
+
+  import { Amplify } from 'aws-amplify';
+  import awsconfig from './aws-exports';
+
+  Amplify.configure(awsconfig);
+</script>
+
 <template>
   <div id="app">
-    <h1>Todo App</h1>
-    <input type="text" v-model="name" placeholder="Todo name" />
-    <input type="text" v-model="description" placeholder="Todo description" />
-    <button v-on:click="createTodo">Create Todo</button>
-    <div v-for="item in todos" :key="item.id">
-      <h3>{{ item.name }}</h3>
-      <p>{{ item.description }}</p>
-    </div>
+    <authenticator>
+    <template v-slot="{ user, signOut }">
+      <div>
+      Hello, {{ user.username }}.
+      <button @click="signOut">Sign Out</button>
+      </div>
+
+      <div style="margin-top: 10px;">
+      <h1>Todo App</h1>
+      <input type="text" v-model="name" placeholder="Todo name" />
+      <input type="text" v-model="description" placeholder="Todo description" />
+      <button v-on:click="createTodo">Create Todo</button>
+      <div v-for="item in todos" :key="item.id">
+        <h3>{{ item.name }}</h3>
+        <p>{{ item.description }}</p>
+      </div>
+      </div>
+    </template>
+    </authenticator>
   </div>
 </template>
 
