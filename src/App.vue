879ac071bf7e33192c032524cfb280067702c8db
murakami77 <murakami77@nifty.com>
2023-05-10 09:35:20 +0900

    todoのサンプルアプリケーションを実装

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,57 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="app">
+    <h1>Todo App</h1>
+    <input type="text" v-model="name" placeholder="Todo name" />
+    <input type="text" v-model="description" placeholder="Todo description" />
+    <button v-on:click="createTodo">Create Todo</button>
+    <div v-for="item in todos" :key="item.id">
+      <h3>{{ item.name }}</h3>
+      <p>{{ item.description }}</p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
+
+<script>
+  import { API } from 'aws-amplify';
+  import { createTodo } from './graphql/mutations';
+  import { listTodos } from './graphql/queries';
+
+  export default {
+    name: 'App',
+    async created() {
+      this.getTodos();
+    },
+    data() {
+      return {
+        name: '',
+        description: '',
+        todos: []
+      };
+    },
+    methods: {
+      async createTodo() {
+        const { name, description } = this;
+        if (!name || !description) return;
+        const todo = { name, description };
+        this.todos = [...this.todos, todo];
+        await API.graphql({
+          query: createTodo,
+          variables: { input: todo }
+        });
+        this.name = '';
+        this.description = '';
+      },
+      async getTodos() {
+        const todos = await API.graphql({
+          query: listTodos
+        });
+        this.todos = todos.data.listTodos.items;
+      }
+    }
+  };
+</script>
 
 <style scoped>
 header {
